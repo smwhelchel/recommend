8adf77db0c9bf1b085f4126e5e984d893e2f80e2
Shannon <smwhelchel@gmail.com>
2016-01-19 15:15:33 -0800

    getting data from node server

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,6 +1,14 @@
 angular.module('recommend')
-   .controller('homeController', function($scope) {
-    vm = this;
-    this.name = "Shannon";
-    this.date = new Date();
+ .controller('homeController', function($http) {
+   vm = this;
+   vm.name = 'Shannon';
+   vm.date = new Date();
+    
+   $http({
+     method: 'GET',
+     url: '/data'
+   }).then(function success(response) {
+     vm.data = response.data.data;
    });
+ });
+
